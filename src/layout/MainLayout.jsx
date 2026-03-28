7a661fa2b2327515be9edc92f664b953ca8e5175
valorflash <sladeshado6@gmail.com>
2026-03-28 21:54:40 +0100

    Fix folder casing for Navbar (Vercel fix)

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,4 +1,4 @@
-import Navbar from "../Components/Navbar/NavbarSection";
+import Navbar from "../Components/temp/NavbarSection";
 import Footer from "../Components/footer/FooterSection";
 import { Outlet } from "react-router-dom";
 
